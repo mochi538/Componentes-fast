33241116867d67bf19da9c265456d98ec711a77d
Onixdrew <andrewmosquera1811@gmail.com>
2024-06-25 18:23:47 -0500

    Primer componente

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import BarraLateralOpciones from "./components/BarraLateral";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <BarraLateralOpciones />
+    </>
+  );
+}
+
+export default App;
